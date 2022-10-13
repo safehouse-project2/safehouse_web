468d3b9e83f397466a13f26012097f1cdfb1efbb
Ishan-sa <ishansach19@gmail.com>
2022-10-13 16:28:52 -0700

    installed talilwind, styled components, MUI with styled components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import React from 'react';
 
 export default function Home() {
 
   return (
     <div className="App">
-      <h1>Safehouse</h1>
+      <h1 className='text-3xl'>Safehouse</h1>
     </div>
   );
 }
