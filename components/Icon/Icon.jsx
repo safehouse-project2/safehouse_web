899d0372d01432085dd934f9d4756d0a7c88d84f
SofiaBCIT <sofia.sirianni@outlook.comm>
2022-10-18 22:22:33 -0700

    icon comp fixed

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 
-import Icon from '@mui/material/Icon';
-
 export default function Icon(){
     return <div>
         <HomeIcon/>
