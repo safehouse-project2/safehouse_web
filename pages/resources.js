6474bd8e9808fb64a345b722bfb2783b708d01a1
SofiaBCIT <ssirianni1@se14l0121-23.ad.bcit.ca>
2022-10-23 18:05:35 -0700

    info card comp

--- pages/resources.js
+++ pages/resources.js
@@ -3,6 +3,7 @@ import { BackgroundContainer, MainContainer, CenterContainer } from '../styles/s
 import AppText from '../components/D3Components/AppText/AppText'
 import Header from '../components/D3Components/Header/Header'
 import Navbar from '../components/D3Components/Navbar/Navbar'
+import InfoCard from '../components/D3Components/InfoCard/InfoCard'
 import { useRouter } from 'next/router'
 
 export default function resources (){
@@ -16,11 +17,11 @@ export default function resources (){
             <div className='flex justify-center items-center flex-col gap-3 mt-50'>
                 <img src="./OnlineWomenTalking.png"/>
             </div>
-            <div className='flex justify-center items-center flex-col gap-3 p-50'>
+            <div className='flex justify-center items-center flex-col gap-5 p-50'>
                 <AppText fontSize="20px" txt="Here are some resources for further assitance"/>
-                <Button txt="FireSmartBC.ca" target="_blank" href="https://firesmartbc.ca"/>
-                <Button txt="Wildfire Preparedness Guide by The BC Goverment" target="_blank" href="https://www2.gov.bc.ca/assets/gov/public-safety-and-emergency-services/emergency-preparedness-response-recovery/embc/preparedbc/preparedbc-guides/wildfire_preparedness_guide.pdf"/>
-                <Button txt="BC Goverment Wildfire Service" target="_blank" href="https://www2.gov.bc.ca/gov/content/safety/wildfire-status"/>
+                <InfoCard txt1="FireSmartBC.ca" txt2="Fire safetey preparedness" target="_blank" href="https://firesmartbc.ca"/>
+                <InfoCard txt1="Wildfire Preparedness Guide" txt2="by The BC Goverment" target="_blank" href="https://www2.gov.bc.ca/assets/gov/public-safety-and-emergency-services/emergency-preparedness-response-recovery/embc/preparedbc/preparedbc-guides/wildfire_preparedness_guide.pdf"/>
+                <InfoCard txt1="BC Goverment Wildfire Service" txt2="Wildfire and evacuation statuses with news and updates" target="_blank" href="https://www2.gov.bc.ca/gov/content/safety/wildfire-status"/>
             </div>
                 <Navbar/>
             </CenterContainer>
