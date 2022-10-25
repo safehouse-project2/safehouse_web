4b013917adf3bf1d818add33e15268c76e1070b2
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-10-24 18:22:42 -0700

    changed storybook

--- components/Home/Detail.stories.js
+++ components/Home/Detail.stories.js
@@ -1,12 +1,17 @@
 import Detail from "./Detail";
+import DetailPage from "../../pages/home/[id]";
 
 export default {
     title: "Detail",
-    component: Detail,
+    component: DetailPage,
     argTypes: {
         state: { state: 'post host information' },
     }
 }
 
-export const Default = (args) => <Detail {...args} />
+export const Default = (args) =>
+    <DetailPage {...args} >
+        <Detail />
+    </DetailPage>
 export const Error = (args) => <Detail {...args} />
+
