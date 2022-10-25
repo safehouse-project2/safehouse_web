5cf6b49b2349d99b6c73caa13aba03c48e51eaf9
Cocoandjane <janeliangbc@gmail.com>
2022-10-24 18:12:01 -0700

    A

--- components/hostPosting/CreateHomePost.stories.js
+++ components/hostPosting/CreateHomePost.stories.js
@@ -1,5 +1,5 @@
 import CreatHomePost from "./CreatHomePost"
-
+import Autocomplete from '../Autocomplete';
 
 export default {
     title: "CreatHomePost",
@@ -11,4 +11,16 @@ export default {
 }
 
 export const Default = (args) => <CreatHomePost {...args} />
+// export const Default = (args) => {
+//     return (
+//         <>
+//             <Script
+//                 strategy="beforeInteractive"
+//                 src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}&libraries=places`}
+//             ></Script>
+//             <CreatHomePost {...args} />
+//         </>
+//     )
+// }
+
 export const Error = (args) => <CreatHomePost {...args} />
