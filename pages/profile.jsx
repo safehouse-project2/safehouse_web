12db03b9dc048396f45e099de85010a53ca7876b
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-10-26 11:03:37 -0700

    changed

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,52 +1,52 @@
-import { BackgroundContainer, CenterContainer } from "../styles/styledComps";
-import Button from "../components/D3Components/Button/Button";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Intro from "../components/D3Components/Profile/intro";
-import Info from "../components/D3Components/Profile/info";
-import Iconbtn from "../components/D3Components/Profile/iconbtn";
-import User from "../components/D3Components/Profile/user";
-import Navbar from "../components/D3Components/Navbar/Navbar";
-import AppText from "../components/D3Components/AppText/AppText";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { useRouter } from "next/router";
-import Link from "next/link";
+// import { BackgroundContainer, CenterContainer } from "../styles/styledComps";
+// import Button from "../components/D3Components/Button/Button";
+// import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+// import Intro from "../components/D3Components/Profile/intro";
+// import Info from "../components/D3Components/Profile/info";
+// import Iconbtn from "../components/D3Components/Profile/iconbtn";
+// import User from "../components/D3Components/Profile/user";
+// import Navbar from "../components/D3Components/Navbar/Navbar";
+// import AppText from "../components/D3Components/AppText/AppText";
+// import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+// import { useRouter } from "next/router";
+// import Link from "next/link";
 
-export default function Listings() {
-  return (
-    <div>
-      <BackgroundContainer src="./homeBG.png">
-        <div className="flex flex-col p-4">
-          <Intro />
-          {/* <User /> */}
-          {/* <Info /> */}
-          <div className="flex p-7">{/* <Iconbtn /> */}</div>
+// export default function Listings() {
+//   return (
+//     <div>
+//       <BackgroundContainer src="./homeBG.png">
+//         <div className="flex flex-col p-4">
+//           <Intro />
+//           {/* <User /> */}
+//           {/* <Info /> */}
+//           <div className="flex p-7">{/* <Iconbtn /> */}</div>
 
-          {/* <CenterContainer>
-            <div className="flex pt-10 pb-20">
-              <Button
-                txt="Contact Host"
-                fontSize="16px"
-                endIcon={<ArrowForwardIcon />}
-                onBtnClick={() => alert("Contacting Host")}
-              />
-            </div>
+//           {/* <CenterContainer>
+//             <div className="flex pt-10 pb-20">
+//               <Button
+//                 txt="Contact Host"
+//                 fontSize="16px"
+//                 endIcon={<ArrowForwardIcon />}
+//                 onBtnClick={() => alert("Contacting Host")}
+//               />
+//             </div>
 
-            <Link href="/list">
-              <AppText
-                color="#F5F5F5"
-                txt=" See All reservations"
-                fontSize="20px"
-              />
-              <KeyboardArrowRightIcon
-                fontSize="large"
-                style={{ fill: "#f5f5f5" }}
-              />
-            </Link>
-          </CenterContainer> */}
-        </div>
+//             <Link href="/list">
+//               <AppText
+//                 color="#F5F5F5"
+//                 txt=" See All reservations"
+//                 fontSize="20px"
+//               />
+//               <KeyboardArrowRightIcon
+//                 fontSize="large"
+//                 style={{ fill: "#f5f5f5" }}
+//               />
+//             </Link>
+//           </CenterContainer> */}
+//         </div>
 
-        <Navbar />
-      </BackgroundContainer>
-    </div>
-  );
-}
+//         <Navbar />
+//       </BackgroundContainer>
+//     </div>
+//   );
+// }
