1cee641e78249c9de71d667bf901605e70be327a
SofiaBCIT <sofia.sirianni@outlook.comm>
2022-10-25 18:59:40 -0700

    icons

--- components/D3Components/Icon/Icon.jsx
+++ components/D3Components/Icon/Icon.jsx
@@ -1,11 +1,12 @@
 import HomeIcon from '@mui/icons-material/Home';
 
+
 export default function Icon({
     icon = <HomeIcon />,
     backgroundColor = "#272727",
     color = "#272727",
 }) {
-    return <>
+    return <div>
         {icon}
-    </>
+    </div>
 }
