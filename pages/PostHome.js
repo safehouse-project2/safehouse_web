9f11f00b9824925c4b36719b1d22068e4b49c761
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-11-08 13:21:00 -0800

    fixed error

--- pages/PostHome.js
+++ pages/PostHome.js
@@ -4,24 +4,27 @@ import CreatHomePost from "../components/hostPosting/CreatHomePost";
 import { Container } from '@mui/material'
 import Script from "next/script";
 import Dropdown from "../components/D3Components/Dropdown/Dropdown";
+import { AuthProvider } from "../AuthContext/AuthContext";
 
 import React from 'react'
 
 export default function PostHome() {
   return (
-    <BackgroundContainer src="homeBG.png">
-      <MainContainer>
-        <LeftContainer>
-          <Script
-            id="my-script"
-            strategy="beforeInteractive"
-            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}&libraries=places`}
-          >
-          </Script>
-          <CreatHomePost />
-        </LeftContainer>
-      </MainContainer>
-    </BackgroundContainer>
+    <AuthProvider>
+      <BackgroundContainer src="homeBG.png">
+        <MainContainer>
+          <LeftContainer>
+            <Script
+              id="my-script"
+              strategy="beforeInteractive"
+              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}&libraries=places`}
+            >
+            </Script>
+            <CreatHomePost />
+          </LeftContainer>
+        </MainContainer>
+      </BackgroundContainer>
+    </AuthProvider>
   )
 }
 
