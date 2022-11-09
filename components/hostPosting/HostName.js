43ea7b76b16e0a92b6b8df4d282c53601d54144b
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-11-08 16:34:33 -0800

    updated host page to show the username

---
+++ components/hostPosting/HostName.js
@@ -0,0 +1,18 @@
+import { useAuth } from "../../AuthContext/AuthContext"
+import AppText from "../D3Components/AppText/AppText";
+export default function HostName() {
+    const { currentUser } = useAuth()
+
+    return (
+        <div>
+            <AppText
+                txt={currentUser.displayName.slice(0, 1).toUpperCase() + currentUser.displayName.slice(1)}
+                fontSize="34px"
+                color="#f5f5f5"
+                fontWeight="bold"
+                lineHeight="1.0"
+                padding="0px 0px 30px 0px"
+            />
+        </div>
+    )
+}
