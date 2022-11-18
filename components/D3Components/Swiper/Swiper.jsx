51f0b28fdcfb1ef2fcba7f05234d86a7e44ad8cd
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-11-17 23:34:01 -0800

    fixed swiper

--- components/D3Components/Swiper/Swiper.jsx
+++ components/D3Components/Swiper/Swiper.jsx
@@ -1,45 +1,42 @@
 // core version + navigation, pagination modules:
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
-import 'swiper/swiper-bundle.css';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 
 SwiperCore.use([Navigation, Pagination]);
 
 export default function SwiperComp({
-    imgSrc = "http://placekitten.com/500/300",
+  imgSrc = "http://placekitten.com/500/300",
 }) {
-    const slides = [];
-    for (let i = 0; i < 5; i += 1) {
-        slides.push(
-            <SwiperSlide key={`slide-${i}`} tag="li">
-                <img
-                    src={imgSrc}
-                    style={{ listStyle: 'none' }}
-                    alt={`Slide ${i}`}
-                />
-            </SwiperSlide>
-        );
-    }
-    return (
-        <>
-            <Swiper
-                tag='section'
-                style={{
-                    width: '100%',
-                }}
-                wrapperTag='ul'
-                id="main"
-                navigation
-                pagination
-                spaceBetween={0}
-                slidesPerView={1}
-                onInit={(swiper) => console.log('Swiper initialized!', swiper)}
-                onSlideChange={(swiper) => console.log('Slide index change to: ', swiper.activeIndex)}
-                onReachEnd={() => console.log('Swiper end reached')}
-            >
-                {slides}
-            </Swiper>
-        </>
-    )
+  const slides = [];
+  for (let i = 0; i < imgSrc.length; i++) {
+    slides.push(
+      <SwiperSlide key={`slide-${i}`} tag="li">
+        <img src={imgSrc[i]} style={{ listStyle: "none" }} alt={`Slide ${i}`} />
+      </SwiperSlide>
+    );
+  }
+  return (
+    <>
+      <Swiper
+        tag="section"
+        style={{
+          width: "100%",
+        }}
+        wrapperTag="ul"
+        id="main"
+        navigation
+        pagination
+        spaceBetween={0}
+        slidesPerView={1}
+        onInit={swiper => console.log("Swiper initialized!", swiper)}
+        onSlideChange={swiper =>
+          console.log("Slide index change to: ", swiper.activeIndex)
+        }
+        onReachEnd={() => console.log("Swiper end reached")}
+      >
+        {slides}
+      </Swiper>
+    </>
+  );
 }
--- components/Home/Detail.jsx
+++ components/Home/Detail.jsx
@@ -10,26 +10,24 @@ import Chat from "../../pages/chat";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Navbar from "../../components/D3Components/Navbar/Navbar";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { doc, getDoc, deleteDoc } from "firebase/firestore";
-import { db } from '../../firebase'
-import Swiper from '../D3Components/Swiper/Swiper'
+import { db } from "../../firebase";
+import Swiper from "../D3Components/Swiper/Swiper";
 
 const Detail = ({ state }) => {
   const router = useRouter();
   const [isEdit, setIsEdit] = useState(false);
   const id = router.query.id;
-  //edit post with user id
   const editHandler = () => {
     setIsEdit(true);
     router.push("/edit/" + id);
     return;
   };
 
-  const buttonHandler = () => { };
+  const buttonHandler = () => {};
 
-  //delete post with user id
   const deleteHandler = () => {
     const docRef = doc(db, "homes", id);
     deleteDoc(docRef);
@@ -40,34 +38,28 @@ const Detail = ({ state }) => {
   return (
     <>
       <div>
-        <div className="absolute w-full h-auto">{/* <NavBar /> */}</div>
-        {state?.image
-          ? state.image.map(it => (
-            <Swiper
-              imgSrc={it ? it : ""}
-            />
-            // <Image src={it ? it : ""} width="2000px" height="1300px" />
-          ))
-          : " No image"}
-
+        <div className="absolute w-full h-auto"></div>
+        <Swiper imgSrc={state.image ? state.image : ""} />
         <div className="flex justify-around border-y-2 border-[#888] py-4 mb-4">
           <div>
             <EditIcon sx={{ color: "#4A4A4A" }} />
-            <button className="text-[#808080]" onClick={editHandler}>Edit Listing</button>
+            <button className="text-[#808080]" onClick={editHandler}>
+              Edit Listing
+            </button>
           </div>
           <div>
             <DeleteIcon sx={{ color: "#4A4A4A" }} />
-            <button className="text-[#808080]" onClick={deleteHandler}>Delete Listing</button>
+            <button className="text-[#808080]" onClick={deleteHandler}>
+              Delete Listing
+            </button>
           </div>
         </div>
-
         <div className="flex flex-col items-left justify-left ml-5">
           <Info state={state} />
           <Description state={state} />
           <Ammenities state={state} />
           <Rules state={state} />
         </div>
-
         <CenterContainer>
           <div className="m-10">
             <Button
@@ -83,30 +75,3 @@ const Detail = ({ state }) => {
   );
 };
 export default Detail;
-
-// function Listings() {
-//   return (
-//     <div key={state.id}>
-//       <div>Address: {state.addressLine1}</div>
-//       <div>City: {state.city}</div>
-//       <div>Province: {state.province}</div>
-//       <div>Postal Code: {state.postalCode}</div>
-//       <div>Description: {state.description}</div>
-//       <div>Guests: {state.guests}</div>
-//       <div>bedrooms: {state.bedrooms}</div>
-//       <div>beds: {state.beds}</div>
-//       <div>bathrooms: {state.bathrooms}</div>
-//       <div>kitchen: {state.kitchen}</div>
-//       <div>parking: {state.parking}</div>
-//       <div>Washer: {state.washer}</div>
-//       <div>Dryer: {state.dryer}</div>
-//       <div>petsAllowed: {state.petsAllowed}</div>
-//       <div>smokingAllowed: {state.smokingAllowed}</div>
-//       <div>Wifi: {state.wifi}</div>
-//       <div>TV: {state.tv}</div>
-//       <div>AC: {state.airConditioning}</div>
-//       <div>Heating: {state.heating}</div>
-//       {state.image && <img src={state.image} />}
-//     </div>
-//   );
-// }
